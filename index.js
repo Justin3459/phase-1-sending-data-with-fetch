be2dcb8592feb453c8bc56973e4fe5a2635bf865
Justin <justin3459@hotmail.com>
2023-04-29 16:16:42 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    }
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(resp => resp.json())
+        .then(obj => {
+            console.log(obj);
+            document.querySelector('body').append(obj.id);
+        })
+        .catch( err => {
+            console.log(err)
+            document.querySelector('body').append(err.message)
+        })
+
+}
+
+submitData();
